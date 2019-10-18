1d1ef12a6f33205cd6580f59a4e27e02e6857d58
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2019-10-18 20:18:53 -0300

    Add desafios 1006, 1007, 1008

---
+++ categorias/iniciante/uri1008.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+numero = int(input())
+horas_trab = int(input())
+valor_por_hora = float(input())
+
+salario = horas_trab * valor_por_hora
+
+print('NUMBER = {}'.format(numero))
+print('SALARY = U$ {:.2f}'.format(salario))
