59cccd266b8cae10572036a331986037d73ad95c
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2019-11-22 01:57:13 -0300

    Add desafios 1186 e 1187

---
+++ categorias/iniciante/uri1186.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+O = input()
+
+m = []
+soma = 0
+col_excluida = 11
+
+for linha in range(12):
+    valores_linha = []
+    for coluna in range(12):
+
+        valores_linha.append(float(input()))
+
+        if coluna > col_excluida:
+            soma += valores_linha[-1]
+
+    col_excluida -= 1
+    m.append(valores_linha)
+
+if O == 'S':
+    print('{:.1f}'.format(soma))
+elif O == 'M':
+    print('{:.1f}'.format(soma / 66))
---
+++ categorias/iniciante/uri1187.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+O = input()
+
+matriz = []
+somatorio, coluna_del = 0, 11
+
+for linha in range(12):
+    valores_da_linha = []
+    for coluna in range(12):
+
+        valores_da_linha.append(float(input()))
+
+        if linha < 5 and coluna > linha and coluna < coluna_del:
+            somatorio += valores_da_linha[-1]
+
+    coluna_del -= 1
+    matriz.append(valores_da_linha)
+
+if O == 'S':
+    print('{:.1f}'.format(somatorio))
+elif O == 'M':
+    print('{:.1f}'.format(somatorio / 30))
