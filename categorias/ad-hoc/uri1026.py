f52cca798e66394aafd55113cf6b3a43e2e7f0ad
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-03-09 18:03:39 -0300

    Refatoração no desafio 1026 da categoria ad-hoc

--- categorias/ad-hoc/uri1026.py
+++ categorias/ad-hoc/uri1026.py
@@ -1,21 +1,8 @@
 # -*- coding: utf-8 -*-    
 
-def format_bin(number):
-    """Formata um número para sua representação binária de 32 bits"""
-    return  '{:0>32b}'.format(number)
-
 while True:
     try:
-        
-        values = map(int, input().split())
-        bin_a, bin_b = map(format_bin, values)
-        
-        new_bin = ''  # Para construção do novo binário
-        for v1, v2 in zip(bin_a, bin_b):
-            if v1 != v2: new_bin += '1'
-            else: new_bin += '0'
-        
-        print(int(new_bin, 2))  # Convertendo o novo binário para decimal
-
+        n1, n2 = map(int, input().split())
+        print(n1^n2)
     except EOFError:
         break
